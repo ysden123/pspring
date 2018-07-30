2344955cd70cda0b66bc80c1fe1fc976f44f5d2d
Yuri Stul <yuriy.s@dauup-clicksmob.com>
2018-07-30 10:10:47 +0300

    SqlDataSource

---
+++ xml-and-annotation/src/main/scala/com/stulsoft/pspring/scala/data/SqlSource.scala
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018. Yuriy Stul
+ */
+
+package com.stulsoft.pspring.scala.data
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.{Component, Service}
+
+/**
+  * @author Yuriy Stul
+  */
+@Component
+class SqlSource extends DataSource {
+  @Autowired
+  val sqlDataProvider: SqlDataProvider = null
+
+  override def data(): String = sqlDataProvider.data()
+}
