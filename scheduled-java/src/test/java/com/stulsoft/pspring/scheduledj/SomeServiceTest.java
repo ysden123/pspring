dda2f051f7af3ce5618104a8aa68622c0a0d8ea5
Yuri Stul <yuriy.s@dauup-clicksmob.com>
2018-10-05 14:50:15 +0300

    SomeServiceTest

---
+++ scheduled-java/src/test/java/com/stulsoft/pspring/scheduledj/SomeServiceTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018. Yuriy Stul
+ */
+
+package com.stulsoft.pspring.scheduledj;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Yuriy Stul
+ */
+public class SomeServiceTest {
+
+    @Test
+    public void doServiceWork() {
+        new SomeService().doServiceWork();
+    }
+}
