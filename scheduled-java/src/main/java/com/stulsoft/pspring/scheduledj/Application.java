cb15a5798db41d7a4f5827a3e3b7f1cae0c68836
Yuri Stul <yuriy.s@dauup-clicksmob.com>
2018-10-05 12:43:35 +0300

    ConditionalOnProperty

--- scheduled-java/src/main/java/com/stulsoft/pspring/scheduledj/Application.java
+++ scheduled-java/src/main/java/com/stulsoft/pspring/scheduledj/Application.java
@@ -9,8 +9,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -20,8 +20,9 @@ import org.springframework.scheduling.annotation.Scheduled;
  */
 @SpringBootApplication(scanBasePackages = {"com.stulsoft.pspring.scheduledj"})
 @PropertySource({"classpath:application.properties"})
-@Configuration
-@Conditional(value = MyCondition.class)
+@ConditionalOnProperty(
+        value = "app.scheduling.enable", havingValue = "true", matchIfMissing = true
+)
 @EnableScheduling
 public class Application implements CommandLineRunner {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
