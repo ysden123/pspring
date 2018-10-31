07030ea73ca1c0cc0816ce1b9bf5e948125801a5
Yuri Stul <yuriy.s@dauup-clicksmob.com>
2018-10-31 12:44:47 +0200

    Standard approach

---
+++ pspring-scala-conf/src/main/scala/com/stulsoft/pspring/scala/conf/standard/StandardApp2Runner.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018. Yuriy Stul
+ */
+
+package com.stulsoft.pspring.scala.conf.standard
+
+import org.springframework.boot.SpringApplication
+
+/**
+  * @author Yuriy Stul
+  */
+object StandardAppRunner {
+  def main(args: Array[String]): Unit = {
+    SpringApplication.run(classOf[StandardApp2], args: _*).close()
+  }
+}
