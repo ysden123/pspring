9bd3e4564e52c38d3db9abf513ad8c6869603c91
Yuriy Stul <ysden123@gmail.com>
2020-08-29 15:24:09 +0300

    web app

---
+++ web-app1/src/main/java/com/stulsoft/pspring/web/app/Application.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020. Yuriy Stul
+ */
+
+package com.stulsoft.pspring.web.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yuriy Stul
+ */
+@SpringBootApplication
+public class Application {
+}
---
+++ web-app1/src/main/java/com/stulsoft/pspring/web/app/ApplicationRunner.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020. Yuriy Stul
+ */
+
+package com.stulsoft.pspring.web.app;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author Yuriy Stul
+ */
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
