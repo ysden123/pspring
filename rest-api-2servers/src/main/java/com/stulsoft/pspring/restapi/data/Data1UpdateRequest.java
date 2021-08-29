47b55abce1e95b7e94d067b444fefb0c4b0518c6
Yuriy Stul <yuriy.s@webpals.com>
2021-08-29 08:36:00 +0300

    rest-api-2servers

---
+++ rest-api-2servers/src/main/java/com/stulsoft/pspring/restapi/data/Data1UpdateRequest.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021. StulSoft
+ */
+
+package com.stulsoft.pspring.restapi.data;
+
+/**
+ * @author Yuriy Stul
+ */
+public record Data1UpdateRequest(String name, Integer age) {}
